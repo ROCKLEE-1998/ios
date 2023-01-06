82013d7e1e4e0be29268d16dacf51177a52103bf
ac0d3r <admin@imipy.com>
2023-01-06 15:18:31 +0800

    update

--- scripts/fix-frida-16.0.8.sh
+++ scripts/fix-frida-16.0.8.sh
@@ -4,6 +4,6 @@ cd frida_16.0.8_iphoneos-arm
 ar -x ../frida_16.0.8_iphoneos-arm.deb
 zstd -d *.zst
 xz *.tar
-ar r frida_16.0.8_iphoneos-arm-repacked.deb debian-binary control.tar.xz data.tar.xz
-# SCP frida_16.0.8_iphoneos-arm-repacked.deb 存档到你的设备，然后运行 dpkg -i：
-# dpkg -i frida_16.0.8_iphoneos-arm-repacked.deb
+ar r frida_16.0.8_iphoneos-arm-zznq.deb debian-binary control.tar.xz data.tar.xz
+# SCP frida_16.0.8_iphoneos-arm-zznq.deb 存档到你的设备，然后运行 dpkg -i：
+# dpkg -i frida_16.0.8_iphoneos-arm-zznq.deb
